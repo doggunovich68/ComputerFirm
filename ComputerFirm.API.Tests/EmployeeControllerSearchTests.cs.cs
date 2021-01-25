de78a42dbf9771b49162595c5edb9949ce0dd50c
Sergey Doggunovich <sharcomovie68@gmail.com>
2021-01-25 13:01:06 +0300

    fourth commit

--- ComputerFirm.API.Tests/EmployeeControllerSearchTests.cs.cs
+++ ComputerFirm.API.Tests/EmployeeControllerSearchTests.cs.cs
@@ -1,6 +1,9 @@
 using System;
 using Xunit;
 using ComputerFirm.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ComputerFirm.API.Tests
 {
@@ -11,5 +14,68 @@ namespace ComputerFirm.API.Tests
         {
             var controller = new EmployeeController();
         }
+	[Fact]
+        public void ItHasSearch() //Проверяем наличие метода Поиск
+        {
+            var controller = new EmployeeController();
+            controller.Search("Jos");
+        }
+	[Fact]
+        public void ItReturnsOkObjectResult()
+        { 
+            var controller = new EmployeeController();
+            var result = controller.Search("Jos");
+            Assert.NotNull(result);                    //Поиск принес результат?
+            Assert.IsType<OkObjectResult>(result);     //Результат ОК?
+        }
+        [Fact]
+        public void ItReturnsCollectionOfEmployee()
+        {
+            var controller = new EmployeeController();
+            var result = controller.Search("Jos") as OkObjectResult;
+            Assert.NotNull(result);
+            Assert.NotNull(result.Value);
+            Assert.IsType<List<Employee>>(result.Value);
+        }
+        [Fact]
+        public void GivenExactMatchThenOneEmployeeInCollection()
+        {
+            var result = _controller.Search("Joshua") as OkObjectResult;
+            var Employees = ((IEnumerable<Employee>)result.Value).ToList();
+            Assert.Equal(1, Employees.Count);
+        }
+	    [Theory]
+	    [InlineData("Joshua")]
+	    [InlineData("joshua")]
+	    [InlineData("JoShUa")]
+	    public void GivenCaseInsensitveMatchThenEmployeeInCollection (string searchString)
+	    {
+		    var result = _controller.Search(searchString) as OkObjectResult;
+		    var Employees = ((IEnumerable<Employee>)result.Value).ToList();
+		    Assert.Equal(1, Employees.Count);
+		    Assert.Equal("Joshua", Employees[0].Name);
+        } 
+        [Fact]
+        public void GivenNoMatchThenEmptyCollection()
+        {
+            var result = _controller.Search("ZZZ") as OkObjectResult;
+            var Employees = ((IEnumerable<Employee>)result.Value).ToList();
+            Assert.Equal(0, Employees.Count);
+        }
+        [Fact]
+        public void Given3MatchThenCollectionWith3Employees()
+        {
+            var result = _controller.Search("jos") as OkObjectResult;
+            var Employees = ((IEnumerable<Employee>)result.Value).ToList();
+            Assert.Equal(3, Employees.Count);
+            Assert.True(Employees.Any(s => s.Name == "Josh"));
+            Assert.True(Employees.Any(s => s.Name == "Joshua"));
+            Assert.True(Employees.Any(s => s.Name == "Joseph"));
+        }
+        private readonly EmployeeController _controller;
+        public EmployeeControllerSearchTests()
+            {
+            _controller = new EmployeeController();
+            }
     }
 }
--- ComputerFirm.API/Controllers/EmployeeController.cs.cs
+++ ComputerFirm.API/Controllers/EmployeeController.cs.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+
 namespace ComputerFirm.API.Controllers
 {
      [Route("api/[controller]")]
      public class EmployeeController : Controller
      {
-      }
+	[HttpGet]
+        [Route("search")]
+        public IActionResult Search(string searchString)
+        {
+            var hardCodedEmployees = new List<Employee>
+            {
+                new Employee {Name = "Josh"},
+                new Employee {Name = "Joshua"},
+                new Employee {Name = "Joseph"},
+                new Employee {Name = "Bill"},
+            };
+
+            var Employees = hardCodedEmployees
+                .Where(x => x.Name.StartsWith(searchString, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            return Ok(Employees);
+        }
+     }
+	 	 public class Employee
+    		{
+       	 		public string Name { get; set; }
+    		}	
 }
